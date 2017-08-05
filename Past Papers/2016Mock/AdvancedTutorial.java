427b0b81b2b1ea7f8ce068f99bd0b4ac64812777
JamesCummiskey <jhcummiskey@gmail.com>
2017-08-05 20:05:06 +0100

    OJ

--- Past Papers/2016Mock/AdvancedTutorial.java
+++ Past Papers/2016Mock/AdvancedTutorial.java
@@ -20,57 +20,6 @@ public class AdvancedTutorial extends Tutorial {
 	}
 
 	public static HashMap<String, ArrayList<AdvancedTutorial>> construct(String[] s) {
-		
-		HashMap<String, ArrayList<AdvancedTutorial>> result = new HashMap<>(); 
-		
-		for(int i=0;i<s.length-2;i++)
-		{
-			AdvancedTutorial addition = new AdvancedTutorial(s[i],s[i+1],s[i+2]);
-		
-			{	if(result.containsKey(s[i])){
-				ArrayList<AdvancedTutorial> a = new ArrayList<>();
-	 			a.addAll(result.get(s[i]));
-	 			a.add(addition);
-				
-				
-			}
-			else
-			{
-				ArrayList<AdvancedTutorial> a = new ArrayList<>();
-				a.add(addition);
-				result.put(s[i], a);
-			}
-				
-			
-			}}
-		
-		 return result;
-		
-	
-	}
-
-	public static void main(String[] args) {
-		
-		String[] a = new String[args.length];
-		if(args.length%3==0&&args.length>0)
-		{
-			   System.out.println(construct(a));
-		}
-		else
-		{
-		  	
-		   System.out.println("Wrong number of arguments!");
-		}
-		
-		/*AdvancedTutorial test1 = new AdvancedTutorial("OP","Tim","0900");
-		AdvancedTutorial test2 = new AdvancedTutorial("OP","Ben","0900");
-		System.out.print(test1.couldCombineWith(test2));
-		*/
-	}
-
-}
-/*
-	public static HashMap<String, ArrayList<AdvancedTutorial>> construct(String[] s) {
 		HashMap<String, ArrayList<AdvancedTutorial>> result = new HashMap<String, ArrayList<AdvancedTutorial>>();
 		// since we're already checking whether or not the array is a multiple
 		// of 3, there's no need
@@ -100,10 +49,6 @@ public class AdvancedTutorial extends Tutorial {
 		return result;
 	}
 
-	public String toString() {
-		return "Topic: " + topic + " " + super.toString();
-	}
-
 	public static void main(String[] args) {
 		String[] a = new String[args.length];
 		for(int i = 0;i<args.length;i++)
@@ -117,4 +62,4 @@ public class AdvancedTutorial extends Tutorial {
 			System.out.println("Wrong number of arguments!");
 		}
 	}
-	*/
+}
