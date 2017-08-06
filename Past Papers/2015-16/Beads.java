8ff0d2164dcfbc7566090c7aa8a818a285fd8860
JOOP2017 <30593207+JOOP2017@users.noreply.github.com>
2017-08-06 15:32:11 +0100

    Update Beads.java

--- Past Papers/2015-16/Beads.java
+++ Past Papers/2015-16/Beads.java
@@ -13,26 +13,28 @@ public class Beads {
 		return nums;
 	}
 
-	public static HashSet<Integer> sums(final ArrayList<Integer> beads, int n) {
-		HashSet<Integer> sums = new HashSet<Integer>();
-		int lengthofArrayList = beads.size();
-		if (n < 1 || lengthofArrayList == 0){
-			return sums;
-		}
-		ArrayList<Integer> line = new ArrayList<>(lengthofArrayList);
-		while (line.size() < (beads.size() + n)){
-			line.addAll(beads);
-		}
-		for (int i = 0; i < lengthofArrayList; i++){
-			int j = i + n;
+	public static Set<Integer> allSums(ArrayList<Integer> beads)
+	{
+		if(beads.size()<1)                                                                                                         
+		{
+			Set<Integer> result = new HashSet<Integer>();
 			
-			int sum = 0;
-			for (int k = i; k < j; k++){
-				sum += line.get(k);
-			}
-		sums.add(sum);
+			
+		return result;
+			
 		}
-		return sums;
+		else
+		{
+			Set<Integer> finalList = new HashSet<Integer>();
+			Set<Integer> addList =new HashSet<Integer>();
+			for(int i=1;i<=beads.size();i++)
+			{ 
+						addList = sums(beads,i);
+			
+			finalList.addAll(addList);
+			}
+			return finalList;
+		}
 	}
 
 	public static int findMax(Set<Integer> beads) {
